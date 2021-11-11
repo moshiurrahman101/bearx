f60df1735d5594f367a4a37e781ae9c6b82484a0
Rm Hridoy <99rmhridoy@gmail.com>
2021-11-11 16:44:32 +0600

    update for 2.0.0

--- src/components/claim/Banner.js
+++ src/components/claim/Banner.js
@@ -1,13 +1,41 @@
 import React from 'react'
-import claimLogo from '../../assets/images/claim.png'
 function Banner() {
     return (
         <div id="claim__banner">
             <div className="container">
                 <div className="claim__main">
-                    <img src={claimLogo} alt="claim now"/>
-                    <h1 className="section__heading">Meet $Claim <br/>BearX Utility Token</h1>
-                    <p>Our vision for SupDucks goes beyond just having the sickest PFP in the game. We also want to leverage our technical powers to fully embrace the blockchain ethos and deliver unrivaled utility for our community.</p>
+                    <h1 className="section__heading">GAME CHANGING TOKENOMICS</h1>
+                    <div className="claim__cards">
+                        <div className="claim__item">
+                            <p>Tokenomics:</p>
+                            <p>TOKEN SYMBOL: $ROOT</p>
+                            <p>DAILY SUPPLY: 37,000</p>
+                            <p>MAX SUPPLY: 135,050,000</p>
+                            <p>REDISTRIBUTION REWARD: 2%</p>                        
+                                                   
+                        </div>
+                        <div className="claim__item">
+                            <p>BURN MECHANICS: NFTs, Events, Digital Collectibles, Project quarterly burns</p>                        
+                            <p>2% Usage Reward (Redistribution)</p>   
+                        </div>
+                        <div className="claim__item">
+                            <p>REWARD MECHANIC: (As it stands), only BearX Genesis series yields 10 $ROOT tokens per day, everyday, for the next 10 years.</p>
+                        </div>
+                        <div className="claim__item"></div>
+                    </div>
+                    
+                    <div className="claim__button">
+                        <button>Connect Wallet</button>
+                        <button>Claim</button>
+                    </div>
+                </div>
+                <div className="claim__content">
+                    <h4>Read more about ROOT here </h4>
+                    <p><a href="https://bearxnft.medium.com">BearX Medium</a></p>
+
+                    <h4>$ROOT TOKEN CLAIM</h4>
+                    <p>Check and claim your $ROOT rewards</p>
+                    <p>The $ROOT token is a significant part of the BearX ecosystem. It fuels the Genesis bears daily with opportunities including reward based challenges, future NFTs, voting rights and more.</p>
                 </div>
             </div>
         </div>
--- src/pages/Lore.js
+++ src/pages/Lore.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import Footer from '../components/Footer'
 import LoreContent from '../components/LoreContent'
-import Navbar from '../components/Navbar'
 
 function Lore() {
     return (
         <div>
-            <Navbar />
             <LoreContent />
-            <Footer />
         </div>
     )
 }
