86bbf50b5e8aa605a3caaced8781236121deba72
Rm Hridoy <99rmhridoy@gmail.com>
2021-11-12 04:10:19 +0600

    Final update 2.0.0

--- src/components/claim/Banner.js
+++ src/components/claim/Banner.js
@@ -9,19 +9,19 @@ function Banner() {
                         <div className="claim__item">
                             <p>Tokenomics:</p>
                             <p>TOKEN SYMBOL: $ROOT</p>
-                            <p>DAILY SUPPLY: 37,000</p>
+                            <p>DAILY SUPPLY: 37,000</p>                    
+                        </div>
+                        <div className="claim__item">
                             <p>MAX SUPPLY: 135,050,000</p>
-                            <p>REDISTRIBUTION REWARD: 2%</p>                        
-                                                   
+                            <p>REDISTRIBUTION REWARD: 2%</p>  
                         </div>
                         <div className="claim__item">
                             <p>BURN MECHANICS: NFTs, Events, Digital Collectibles, Project quarterly burns</p>                        
-                            <p>2% Usage Reward (Redistribution)</p>   
+                            <p>2% Usage Reward (Redistribution)</p> 
                         </div>
                         <div className="claim__item">
                             <p>REWARD MECHANIC: (As it stands), only BearX Genesis series yields 10 $ROOT tokens per day, everyday, for the next 10 years.</p>
                         </div>
-                        <div className="claim__item"></div>
                     </div>
                     
                     <div className="claim__button">
